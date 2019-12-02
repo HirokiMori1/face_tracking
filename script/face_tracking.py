590d84f695d16635dfd93cea6a259760f9a06a8c
HirokiMori1 <h.mori@kufusha.com>
2019-12-02 11:06:28 +0900

    add tiltmove

--- script/face_tracking.py
+++ script/face_tracking.py
@@ -14,10 +14,16 @@ from control_msgs.msg import JointTrajectoryControllerState as JTCS
 
 
 #X軸の角度の変化とpixelの変化の関数　x[pixel] = 479 * rad
-#Y軸の角度の変化とpixelの変化の関数　y[pixel] = 
+#Y軸の角度の変化とpixelの変化の関数　y[pixel] = 475 * rad
 
 robot = hsrb_interface.Robot()
 whole_body = robot.get('whole_body')
+
+min_pan_joint = -3.8
+max_pan_joint = 1.7
+
+min_tilt_joint = -1.5
+max_tilt_joint = 0.52
 
 class FaceTracking:
     def __init__(self):
@@ -44,17 +50,34 @@ class FaceTracking:
         #X軸方向に頭を動かす角度を計算[rad]
         delta_pan_joint = (self.image_center_x - self.face_point_x) / 479.0 
         #Y軸方向に頭を動かす角度を計算[rad]
-        delta_tilt_joint = 0.0
+        delta_tilt_joint = (self.image_center_y - self.face_point_y) / 475.0
 
         if -0.03 <= delta_pan_joint and delta_pan_joint <= 0.03:
-			delta_pan_joint = 0
+			delta_pan_joint = 0.0
+        if -0.03 <= delta_tilt_joint and delta_tilt_joint <= 0.03:
+            delta_tilt_joint = 0.0
+            
         #現在のheadの角度を取得
         #joint_names: [head_tilt_joint, head_pan_joint]
         current_head_joint = rospy.wait_for_message("/hsrb/head_trajectory_controller/state", JTCS, timeout=None)
         current_pan_joint = current_head_joint.desired.positions[1] #[rad]
         current_tilt_joint = current_head_joint.desired.positions[0] #[rad]
 
-        whole_body.move_to_joint_positions({'head_pan_joint': current_pan_joint + delta_pan_joint, 'head_tilt_joint': current_tilt_joint + delta_tilt_joint})
+        move_pan_joint = current_pan_joint + delta_pan_joint
+        move_tilt_joint = current_tilt_joint + delta_tilt_joint
+
+        #頭を動かす角度を制限
+        if move_pan_joint <= min_pan_joint:
+            move_pan_joint = min_pan_joint
+        elif move_pan_joint >= max_pan_joint:
+            move_pan_joint = max_pan_joint
+
+        if move_tilt_joint < min_tilt_joint:
+            move_tilt_joint = min_tilt_joint
+        elif move_tilt_joint >= max_tilt_joint:
+            move_tilt_joint = max_tilt_joint
+
+        whole_body.move_to_joint_positions({'head_pan_joint': move_pan_joint, 'head_tilt_joint': move_tilt_joint})
 
 
 if __name__ == '__main__':
